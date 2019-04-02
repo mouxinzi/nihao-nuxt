607ddf41156bd4587f25151b99090a1cd2e34449
t.k <408509669@qq.com>
2019-04-02 15:29:23 +0800

    '使用vuex'

---
+++ store/demo.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+  pageName: 'Vuex',
+  count:0
+})
+
+export const mutations = {
+  add(state, number){
+    state.count =state.count + number
+  }
+}
+
+export const actions ={
+  addAction(context,number){
+    
+    setTimeout(() => {
+      context.commit('add', number)
+    }, 1000);
+  }
+}
